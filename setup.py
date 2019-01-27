de6d67cc127b679a825c395642d16779617af8b8
ctomkow <ctomkow@gmail.com>
2019-01-26 21:25:38 -0800

    pip3 install fix

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pwned',
-    version='0.1',
+    version='0.01',
     url='https://github.com/ctomkow/pwned',
     author='Craig Tomkow',
     author_email='ctomkow@gmail.com',
     license='MIT',
-    packages=['pwned'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['lastpass-python', 'click', 'requests'],
     entry_points={
           'console_scripts': [
